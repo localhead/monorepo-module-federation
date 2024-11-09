ad1490e373cda87b4885d8d24f4e622d16a719dd
Bula Rahman <dodoalive1994@gmail.com>
2024-11-09 12:59:48 +0500

    init

--- dist/config/build/buildDevServers.js
+++
@@ -1,9 +0,0 @@
-export function buildDevServers(options) {
-    var _a;
-    return {
-        port: (_a = options.port) !== null && _a !== void 0 ? _a : 5000,
-        open: true,
-        historyApiFallback: true, // This line is important for React Router
-        hot: true,
-    };
-}
--- dist/src/packages/icons/index.js
+++
@@ -1,3 +0,0 @@
-import { createIconFromSvg } from "@utils/createIconFromSvg";
-import AlarmSvg from "./assets/alarm.svg";
-export var AlarmIcon = createIconFromSvg(AlarmSvg);
